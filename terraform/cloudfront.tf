a2780dccbca204b8f1cd80ae525b0bbc8263ee71
PH Oliveira <132354546+phmoliv@users.noreply.github.com>
2025-11-13 15:00:27 -0300

    Add CloudFront distribution configuration

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,89 @@
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    #origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Henlaços webpage"
+  default_root_object = "home.html"
+
+  #logging_config {
+  #  include_cookies = false
+  #  bucket          = "mylogs.s3.amazonaws.com"
+  #  prefix          = "myprefix"
+  #}
+
+  aliases = ["henlacos.click", "www.henlacos.click"]
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    #viewer_protocol_policy = "allow-all"
+    #min_ttl                = 0
+    #default_ttl            = 3600
+    #max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  # When to choose what:
+  # PriceClass_All → Best latency globally (use if users are worldwide).
+  # PriceClass_200 → Balanced performance & cost (good default).
+  # PriceClass_100 → Cheapest, but bad for users outside US/Canada/Europe.
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Project = "Henlacos"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validated.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  depends_on = [aws_s3_bucket.site]
+}
