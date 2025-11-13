81751f6f62157d285730cdc0dd433923f94b4a24
PH Oliveira <132354546+phmoliv@users.noreply.github.com>
2025-11-13 15:21:53 -0300

    Add default values to domain_name and route53_zone_id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,10 +6,13 @@ variable "bucket_name" {
 
 variable "domain_name" {
   description = "Main Domain"
+  default     = "henlacos.click"
   type        = string
 }
 
 variable "route53_zone_id" {
   description = "Route53 Zone ID"
+  default     = "Z01046291JU5F2O88XLB7"
   type        = string
+  sensitive   = true
 }
