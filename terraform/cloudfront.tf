616c1d8e9c44e04b29415f19ec6d77a79fd07d2d
PH Oliveira <132354546+phmoliv@users.noreply.github.com>
2025-11-13 15:53:48 -0300

    Restrict allowed methods for CloudFront distribution

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -20,7 +20,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   aliases = ["henlacos.click", "www.henlacos.click"]
 
   default_cache_behavior {
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "redirect-to-https"
