68fa14cac50f0431dfba06d033ec1f8e90e40a23
PH Oliveira <132354546+phmoliv@users.noreply.github.com>
2025-11-13 15:19:43 -0300

    Simplify S3 bucket configuration
    Removed error document and routing rules from S3 bucket configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,19 +12,6 @@ resource "aws_s3_bucket_website_configuration" "site" {
   index_document {
     suffix = "home.html"
   }
-
-  error_document {
-    key = "error.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
 }
 
 resource "aws_s3_bucket_policy" "public_read" {
